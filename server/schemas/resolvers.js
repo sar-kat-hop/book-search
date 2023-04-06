6fb641facfae3aa87f7875a151059d9f3f277c85
Sarah <sarah.k.hopper@gmail.com>
2023-04-05 22:28:45 -0700

    update resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,22 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
 
-const resolver = {
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.find({}).populate('savedBooks');
+            }
+            throw new AuthenticationError('Please log in to view your saved books.');
+        },
+    },
+// query for searching for books api call?
+    Query: {
+        books: async () => {
+            return await Book.find({});
+        }
+    }
 };
 
 module.exports = resolvers;
